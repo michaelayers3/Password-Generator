3951c4530da87b6612e1b34e772bccbbd56d8626
michaelayers3 <michaelgregoryayers@gmail.com>
2023-03-10 17:08:43 -0700

    added a bunch of math stuff. I'm struggling on how to change the amount of each character type according to length and if other variables are present. I'm sure there's an easy way to do it but I'm also sure I'll over complicate it. Calling it here for the day though.

--- Assets/script.js
+++ Assets/script.js
@@ -17,43 +17,54 @@ function generatePassword(){
     var Length = parseInt(response);
     console.log(Length);
   }
-  
+ 
   var lowerCase = confirm("Do you want lowercase letters?");
     if(lowerCase === true){
-      var index= Math.floor(Math.random(Length) * optionsLower.length);
-      var randomLower = optionsLower[index];}
-    }
+      for(var i = 0; i < Length; i++){
+      var index= Math.floor(Math.random() * optionsLower.length);
+      var randomLower= optionsLower[index];
       console.log(randomLower);
+  }
+   } else{
+    var randomLower= ""}
 
+  var upperCase = confirm("Do you want uppercase letters?");
+    if(upperCase === true){
+      for(var i = 0; i < Length; i++){
+      var index= Math.floor(Math.random() * optionsUpper.length);
+      var randomUpper= (optionsUpper[index]);
+      console.log(randomUpper);
+  }
+  } else{
+    var randomUpper= "";}
 
+  var numbers = confirm("Do you want numbers?");
+    if(numbers === true){
+      for(var i = 0; i < Length; i++){
+      var index= Math.floor(Math.random() * optionsNumbers.length);
+      var randomNumbers= optionsNumbers[index];
+      console.log(randomNumbers);
+      }} else{
+        var randomNumbers= "";}
+  var special = confirm("Do you want special characters?");
+    if(special === true){
+      for(var i = 0; i < Length; i++){
+      var index= Math.floor(Math.random() * optionsSpecial.length);
+      var randomSpecial= optionsSpecial[index];
+      console.log(randomSpecial);
+    }} else{
+      var randomSpecial= "";}
 
-  // var upperCase = confirm("Do you want uppercase letters?");
-  //   if(upperCase === true){
-  //     var index= Math.floor(Math.random() * optionsUpper.length);
-  //     var randomUpper= Length * optionsUpper;
-  //   } else{
-  //     var randomUpper= password += "";
-  //   }
-  // var numbers = confirm("Do you want numbers?");
-  //   if(numbers === true){
-  //     var index= Math.floor(Math.random() * optionsNumbers.length);
-  //     var randomNumbers= Length * optionsNumbers;
-  //   } else{
-  //     var randomNumbers= password += "";
-  //   }
-  // var special = confirm("Do you want special characters?");
-  //   if(special === true){
-  //     var index= Math.floor(Math.random() * optionsSpecial.length);
-  //     var randomSpecial= Length * optionsSpecial;
-  //   } else{
-  //     var randomSpecial= password += "";
-  //   }
-
-  // if (lowerCase === false && upperCase === false && numbers === false && special === false) {
-  //   alert("Please select at least one character type");
-  //   return;
-  // }
+  if (lowerCase === false && upperCase === false && numbers === false && special === false) {
+    alert("Please select at least one character type");
+  }
+var password = randomLower+ randomUpper+ randomNumbers+ randomSpecial;
+    for(var i = Length; i < Length; i++){
+    //   var index= Math.floor(Math.random() * together.length);
+    //   var randomTogether= together[index];
+      console.log(password);
     
+};
 
 
 // Write password to the #password input
@@ -64,7 +75,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+writePassword();
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
   
